9278664383822a198a2e3ebec9af339b110e953f
Fernando Boueres <132353494+fboueres@users.noreply.github.com>
2025-09-03 22:52:35 -0300

    edges case 14:49

--- src/domain/value_objects/date_range.ts
+++ src/domain/value_objects/date_range.ts
@@ -3,7 +3,10 @@ export class DateRange {
     private readonly endDate: Date;
 
     constructor(startDate: Date, endDate: Date) {
-        if (endDate <= startDate) {
+        if (endDate == startDate) {
+            throw new Error('A data de ínicio e término não podem ser iguais.');
+        }
+        if (endDate < startDate) {
             throw new Error('A data de término deve ser posterior à data de ínicio.');
         }
 
@@ -22,5 +25,12 @@ export class DateRange {
     getTotalNights(): number {
         const diffTime = this.endDate.getTime() - this.startDate.getTime();
         return Math.ceil(diffTime / (1000 * 3600 * 24));
+    }
+
+    overlaps(other: DateRange): boolean {
+        return (
+            this.startDate < other.endDate &&
+            other.getStartDate() < this.endDate            
+        );
     }
 }
